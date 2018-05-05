101f2918f4f6e23fa29874ee4f4013417b2a8a9d
Colin Heath <colinjhe@usc.edu>
2018-05-04 19:45:35 -0700

    Began writing GTest suite for Deck class.

---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include "Deck.h"
+#include "gtest/gtest.h"
+
+TEST(DeckTest, ConstructPresent)
+{
+	Deck deck;
+
+	
+}
+
+TEST(DeckTest, ConstructOrder)
+{
+	Deck deck;
+
+
+}
+
+TEST(DeckTest, DealRepeat)
+{
+	Deck deck;
+
+
+}
+
+TEST(DeckTest, DealLimit)
+{
+	Deck deck;
+
+
+}
+
+TEST(DeckTest, ShuffleRepeat)
+{
+	Deck deck;
+
+
+}
